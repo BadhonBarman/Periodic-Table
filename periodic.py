e7a616131b67538f9b667b4c56b8af79abc3d2ba
Badhon Barman <58289031+BadhonBarman@users.noreply.github.com>
2020-11-13 19:49:45 +0600

    Create periodic.py

---
+++ periodic.py
@@ -0,0 +1,178 @@
+giveme = input("Symbol of the element: ").lower().strip()
+
+
+
+
+#START ROW 1
+#HYDROGEN
+if giveme == "h":
+	print("position = Row:1, Colum:1" + 
+		"\nIt's HYDROGEN".upper() + 
+		"\nAtomic mass:1".title() + 
+		"\nAtomic number:1".title() +"\nELECTRON CONFIGURATION:1s1")
+
+#HELIUM
+elif giveme == "he":
+	print("position = Row:1, Colum:18" +
+	 "\nIt's HELIUM".upper() + 
+	 "\nAtomic mass:4".title() + 
+	 "\nAtomic number:2".title() +"\nELECTRON CONFIGURATION:1s2")
+#END ROW 1
+#START ROW 2 
+#lithium
+elif giveme == "li":
+	print("position = Row:2, Colum:1" + 
+		"\nIt's lithium".upper() + 
+		"\nit's Alkali metal".upper() +
+		"\nAtomic mass:7".title() + 
+		"\nAtomic number:3".title() +"\nELECTRON CONFIGURATION:1s2 2s1")
+#Beryllium
+elif giveme == "be":
+	print("position = Row:2, Colum:2" + 
+		"\nit's alkaline earth metal".upper() +
+		"\nIt's Beryllium".upper() + 
+		"\nAtomic mass:9".title() + 
+		"\nAtomic number:4".title() +"\nELECTRON CONFIGURATION:1s2 2s2")
+#Boron
+elif giveme == "b":
+	print("position = Row:2, Colum:13" + 
+		"\nIt's Boron".upper() + 
+		"\nAtomic mass:11".title() + 
+		"\nAtomic number:5".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p1")
+
+#CARBON
+elif giveme == "c":
+	print("position = Row:2, Colum:14" + 
+		"\nIt's carbon".upper() + 
+		"\nAtomic mass:12".title() + 
+		"\nAtomic number:6".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p2")
+
+#nitrogen
+elif giveme == "n":
+	print("position = Row:2, Colum:15" + 
+		"\nIt's nitrogen".upper() + 
+		"\nAtomic mass:14".title() + 
+		"\nAtomic number:7".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p3")
+
+#Oxygen
+elif giveme == "o":
+	print("position = Row:2, Colum:16" + 
+		"\nIt's Oxygen".upper() + 
+		"\nAtomic mass:16".title() + 
+		"\nAtomic number:8".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p4")
+
+#Fluorine
+elif giveme == "f":
+	print("position = Row:2, Colum:17" + 
+		"\nIt's Fluorine".upper() + 
+		"\nAtomic mass:19".title() + 
+		"\nAtomic number:9".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p5")
+
+#Neon
+elif giveme == "ne":
+	print("position = Row:2, Colum:18" + 
+		"\nIt's Neon".upper() + 
+		"\nAtomic mass:20".title() + 
+		"\nAtomic number:10".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p6")
+
+#END ROW 2
+#START ROW 3
+#Sodium
+elif giveme == "na":
+	print("position = Row:3, Colum:1" + 
+		"\nit's Alkali metal".upper() +
+		"\nIt's Sodium".upper() + 
+		"\nAtomic mass:23".title() + 
+		"\nAtomic number:11".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p6 3s1")
+
+#Magnesium
+elif giveme == "mg":
+	print("position = Row:3, Colum:2" + 
+		"\nIt's Magnesium".upper() + 
+		"\nAtomic mass:24".title() + 
+		"\nAtomic number:12".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p6 3s2")
+
+#Aluminum
+elif giveme == "al":
+	print("position = Row:3, Colum:13" + 
+		"\nIt's Aluminum".upper() + 
+		"\nAtomic mass:27".title() + 
+		"\nAtomic number:13".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p6 3s2 3p1")
+
+#Silicon
+elif giveme == "si":
+	print("position = Row:3, Colum:14" + 
+		"\nIt's Silicon".upper() + 
+		"\nAtomic mass:28".title() + 
+		"\nAtomic number:14".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p6 3s2 3p2")
+
+#Phosphorus
+elif giveme == "p":
+	print("position = Row:3, Colum:15" + 
+		"\nIt's Phosphorus".upper() + 
+		"\nAtomic mass:31".title() + 
+		"\nAtomic number:15".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p6 3s2 3p3")
+
+#Sulfur
+elif giveme == "s":
+	print("position = Row:3, Colum:16" + 
+		"\nIt's Sulfur".upper() + 
+		"\nAtomic mass:32".title() + 
+		"\nAtomic number:16".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p6 3s2 3p4")
+
+#Chlorine
+elif giveme == "cl":
+	print("position = Row:3, Colum:17" + 
+		"\nIt's Chlorine".upper() + 
+		"\nAtomic mass:35.5".title() + 
+		"\nAtomic number:17".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p6 3s2 3p5")
+
+#Argon
+elif giveme == "ar":
+	print("position = Row:3, Colum:18" + 
+		"\nIt's Argon".upper() + 
+		"\nAtomic mass:40".title() + 
+		"\nAtomic number:18".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p6 3s2 3p6")
+
+#end of row 3 
+#start row 4
+
+#Potassium
+elif giveme == "k":
+	print("position = Row:4, Colum:1" + 
+		"\nIt's Potassium".upper() + 
+		"\nAtomic mass:39".title() + 
+		"\nAtomic number:19".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p6 3s2 3p6 4s1")
+
+#Calcium
+elif giveme == "ca":
+	print("position = Row:4, Colum:2" + 
+		"\nIt's Calcium".upper() + 
+		"\nAtomic mass:40".title() + 
+		"\nAtomic number:20".title() +"\nELECTRON CONFIGURATION:1s2 2s2 2p6 3s2 3p6 4s2")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+else:
+	print("something worng!" +
+	     "\nplease reload and type the Symbol of element correctly")
+
+
+
